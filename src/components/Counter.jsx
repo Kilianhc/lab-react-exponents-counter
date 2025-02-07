c0eae3389910b7f9b135154391de93d2eab45640
Kilian H <kilianhc@hotmail.com>
2025-02-07 12:45:39 +0000

    done

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const decrement = () => setCount((prevCount) => prevCount - 1);
-  const increment = () => setCount((prevCount) => prevCount + 1);
+export default function Counter({count, increment, decrement}) {
 
   return (
     <div className="counter-container">
@@ -13,6 +9,5 @@ const Counter = () => {
       <button className="counter-button" onClick={increment}>+</button>
     </div>
   );
-};
+}
 
-export default Counter;
---
+++ src/components/Exponent.jsx
@@ -0,0 +1,8 @@
+export default function Exponent({ num, exponent }) {
+    return (
+        <div className="exponent-counter-container">
+            <p className="exponent-label">{num}^{exponent}</p>
+            <p className="exponent-result">{num} * {num} * {num} * {num} = <span className="total">{num ** 4}</span></p>
+        </div>
+    )
+}
