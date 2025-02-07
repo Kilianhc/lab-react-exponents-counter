7f3865d6199b2a4a7933176c11e322afe39362c7
Kilian D. Hernández Chirino <kilianhc@hotmail.com>
2025-02-07 12:51:13 +0000

    Update Exponent.jsx

--- src/components/Exponent.jsx
+++ src/components/Exponent.jsx
@@ -2,7 +2,7 @@ export default function Exponent({ num, exponent }) {
     return (
         <div className="exponent-counter-container">
             <p className="exponent-label">{num}^{exponent}</p>
-            <p className="exponent-result">{num} * {num} * {num} * {num} = <span className="total">{num ** 4}</span></p>
+            <p className="exponent-result">{num} * {num} * {num} * {num} = <span className="total">{num ** {exponent}</span></p>
         </div>
     )
 }
